ffb07c16bb6e7345732771194f08ae2a3c7b9cf0
KPorus <designerfardin2021@gmail.com>
2023-01-12 22:03:52 +0600

    practice button sx and typegraphy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Add, Settings } from '@mui/icons-material';
 import { Button, Typography } from '@mui/material';
 
 function App() {
   return (
     <div>
       <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
-      <Typography variant="h1" className='text-center uppercase'>
+      <Button variant='contained' color='secondary' startIcon={<Settings />}>
+        Settings
+      </Button>
+      <Button variant='contained' color='success' size='small' startIcon={<Add/>}>
+        Add Profile
+      </Button>
+      <Button variant='contained' sx={{
+        backgroundColor:"skyblue",
+        color:"red",
+        "&:hover":
+        {
+          backgroundColor:"orange",
+          color:"white",
+        }
+      }}>My unique button</Button>
+      <Typography variant='h1' className='text-center uppercase'>
         hello world
       </Typography>
     </div>
