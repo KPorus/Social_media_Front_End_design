dfaa7d98ccbb2d02c4a6e30d1f61206c9cc21e27
KPorus <designerfardin2021@gmail.com>
2023-01-12 22:07:55 +0600

    componant button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
+import styled from '@emotion/styled';
 import { Add, Settings } from '@mui/icons-material';
 import { Button, Typography } from '@mui/material';
 
 function App() {
+  const TestButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "red",
+    "&:hover": {
+      backgroundColor: "orange",
+      color: "white",
+    },
+  });
   return (
     <div>
       <Button variant='text'>Text</Button>
       <Button variant='contained' color='secondary' startIcon={<Settings />}>
         Settings
       </Button>
-      <Button variant='contained' color='success' size='small' startIcon={<Add/>}>
+      <Button
+        variant='contained'
+        color='success'
+        size='small'
+        startIcon={<Add />}>
         Add Profile
       </Button>
-      <Button variant='contained' sx={{
-        backgroundColor:"skyblue",
-        color:"red",
-        "&:hover":
-        {
-          backgroundColor:"orange",
-          color:"white",
-        }
-      }}>My unique button</Button>
+      <Button
+        variant='contained'
+        sx={{
+          backgroundColor: "skyblue",
+          color: "red",
+          "&:hover": {
+            backgroundColor: "orange",
+            color: "white",
+          },
+        }}>
+        My unique button
+      </Button>
       <Typography variant='h1' className='text-center uppercase'>
         hello world
       </Typography>
+      <TestButton>My test button</TestButton>
     </div>
   );
 }
