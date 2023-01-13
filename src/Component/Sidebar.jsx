e47119706ee15b1f80178b36b9001c3b6c3c348c
KPorus <designerfardin2021@gmail.com>
2023-01-13 14:05:13 +0600

    side bar setup

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -1,36 +1,101 @@
-import { AccountCircle, Home } from '@mui/icons-material';
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { Box } from '@mui/system';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {
+  AccountCircle,
+  Article,
+  Groups,
+  Home,
+  Label,
+  ModeNight,
+  PersonAdd,
+  Settings,
+} from "@mui/icons-material";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-      <Box p={2} flex={2} sx={{ display: { xs: "none", sm: "block" } }}>
-        <List>
-          <Link to="/">
-            <ListItem disablePadding components='a'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Home />
-                </ListItemIcon>
-                <ListItemText primary='Homepage' />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to='/profile'>
-            <ListItem disablePadding components='a'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <AccountCircle />
-                </ListItemIcon>
-                <ListItemText primary='Profile' />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-        </List>
-      </Box>
-    );
+  return (
+    <Box p={2} flex={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <Link to='/'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary='Homepage' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to='/'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary='Pages' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to='/'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <Groups />
+              </ListItemIcon>
+              <ListItemText primary='Group' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to='/'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <PersonAdd />
+              </ListItemIcon>
+              <ListItemText primary='Friends' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to='/'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary='Settings' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to='/profile'>
+          <ListItem disablePadding components='a'>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccountCircle />
+              </ListItemIcon>
+              <ListItemText primary='Profile' />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+
+        <ListItem disablePadding components='a'>
+          <ListItemButton>
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 };
 
 export default Sidebar;
