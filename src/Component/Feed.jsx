48d49e98d91d8ea661f3e3889d2fd4bbf0754468
KPorus <designerfardin2021@gmail.com>
2023-01-14 20:19:21 +0600

    skeleton'

--- src/Component/Feed.jsx
+++ src/Component/Feed.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { Box } from '@mui/system';
 import React from 'react';
 import Post from './Post';
+import { Skeleton, Stack } from '@mui/material';
 
 const Feed = () => {
 //   const [count, setCount] = useState(0);
@@ -9,15 +11,30 @@ const Feed = () => {
 //     let c = count +1;
 //     setCount(c);
 //   }
+const [loading, setLoading] = useState(true);
+
+setTimeout(() => {
+  setLoading(false);
+}, [3000]);
     return (
-      <Box flex={4} marginX={2} p={2} >
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
+      <Box flex={4} marginX={2} p={2}>
+        {loading ? (
+          <Stack spacing={1}>
+            <Skeleton variant='text' height={100} />
+            <Skeleton variant='text' height={20} />
+            <Skeleton variant='text' height={20} />
+            <Skeleton variant='rectangular' height={300} />
+          </Stack>
+        ) : (
+          <>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </>
+        )}
       </Box>
     );
 };
